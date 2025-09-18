e03692470267fdba224ab08049d7bb954ec792c5
Nitin10cd <nsaxenacse@gmail.com>
2025-09-18 10:36:17 +0530

    LSP wrong implementation to understand the concept how the lsp work in this lsp is violated

---
+++ SOLID_Design_patterns/open-close-principle.cpp
@@ -0,0 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// ? what is this : a classs is open for extesion but close for modification
+// for integrating the new feature no need to update the existing classes

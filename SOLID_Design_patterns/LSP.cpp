b1a35d0cafbaf39d1f5530fd74757d2069575847
Nitin10cd <nsaxenacse@gmail.com>
2025-09-18 10:38:02 +0530

    LSP corect implementation

--- SOLID_Design_patterns/LSP.cpp
+++ SOLID_Design_patterns/LSP.cpp
@@ -5,13 +5,21 @@
 
 using namespace std;
 
-class Account {
+
+
+// main class from which the cloent is communicate 
+// sets the account 
+class DepositOnlyAccount {
 public:
     virtual void deposit(double amount) = 0;
+};
+
+class WithdrawableAccount : public DepositOnlyAccount {
+public:
     virtual void withdraw(double amount) = 0;
 };
 
-class SavingAccount : public Account {
+class SavingAccount : public WithdrawableAccount {
 private:
     double balance;
 
@@ -35,85 +43,201 @@ public:
     }
 };
 
-class CurrentAccount : public Account {
-private:
-    double balance;
-
-public:
-    CurrentAccount() { 
-        balance = 0; 
-    }
-
-    void deposit(double amount) {
-        balance += amount;
-        cout << "Deposited: " << amount << " in Current Account. New Balance: " << balance << endl;
-    }
-
-    void withdraw(double amount) {
-        if (balance >= amount) {
-            balance -= amount;
-            cout << "Withdrawn: " << amount << " from Current Account. New Balance: " << balance << endl;
-        } else {
-            cout << "Insufficient funds in Current Account!\n";
+class CurrentAccount : public WithdrawableAccount {
+    private:
+        double balance;
+    
+    public:
+        CurrentAccount() { 
+            balance = 0; 
         }
-    }
-};
+    
+        void deposit(double amount) {
+            balance += amount;
+            cout << "Deposited: " << amount << " in Current Account. New Balance: " << balance << endl;
+        }
+    
+        void withdraw(double amount) {
+            if (balance >= amount) {
+                balance -= amount;
+                cout << "Withdrawn: " << amount << " from Current Account. New Balance: " << balance << endl;
+            } else {
+                cout << "Insufficient funds in Current Account!\n";
+            }
+        }
+    };
 
-class FixedTermAccount : public Account {
+class FixedTermAccount : public DepositOnlyAccount {
 private:
     double balance;
 
 public:
     FixedTermAccount() { 
-        balance = 0;
+        balance = 0; 
     }
 
     void deposit(double amount) {
         balance += amount;
         cout << "Deposited: " << amount << " in Fixed Term Account. New Balance: " << balance << endl;
     }
-
-    void withdraw(double amount) {
-        throw logic_error("Withdrawal not allowed in Fixed Term Account!");
-    }
 };
 
-//Client class
 class BankClient {
 private:
-    vector<Account*> accounts;
+    vector<WithdrawableAccount*> withdrawableAccounts;
+    vector<DepositOnlyAccount*> depositOnlyAccounts;
 
 public:
-    BankClient(vector<Account*> accounts) { 
-        this->accounts = accounts; 
+    BankClient( vector<WithdrawableAccount*> withdrawableAccounts, 
+        vector<DepositOnlyAccount*> depositOnlyAccounts) { 
+        this->withdrawableAccounts = withdrawableAccounts; 
+        this->depositOnlyAccounts = depositOnlyAccounts;
     }
 
     void processTransactions() {
-        for (Account* acc : accounts) {
+        for (WithdrawableAccount* acc : withdrawableAccounts) {
             acc->deposit(1000);
-
-            //Checking account type explicitly
-            if (typeid(*acc) == typeid(FixedTermAccount)) {
-                cout << "Skipping withdrawal for Fixed Term Account.\n";
-            } else {
-                try {
-                    acc->withdraw(500);
-                } catch (const logic_error& e) {
-                    cout << "Exception: " << e.what() << endl;
-                }
-            }
+            acc->withdraw(500); 
+        }
+        for (DepositOnlyAccount* acc : depositOnlyAccounts) {
+            acc->deposit(5000);
         }
     }
 };
 
 int main() {
-    vector<Account*> accounts;
-    accounts.push_back(new SavingAccount());
-    accounts.push_back(new CurrentAccount());
-    accounts.push_back(new FixedTermAccount());
+    vector<WithdrawableAccount*> withdrawableAccounts;
+    withdrawableAccounts.push_back(new SavingAccount());
+    withdrawableAccounts.push_back(new CurrentAccount());
 
-    BankClient* client = new BankClient(accounts);
+    vector<DepositOnlyAccount*> depositOnlyAccounts;
+    depositOnlyAccounts.push_back(new FixedTermAccount());
+
+    BankClient* client = new BankClient (withdrawableAccounts, depositOnlyAccounts);
     client->processTransactions();
 
     return 0;
 }
+
+
+// WRONG IMPLEMENTATION => TIGHTLY COUPLED
+
+// #include <iostream>
+// #include <vector>
+// #include <typeinfo>
+// #include <stdexcept>
+
+// using namespace std;
+
+// class Account {
+// public:
+//     virtual void deposit(double amount) = 0;
+//     virtual void withdraw(double amount) = 0;
+// };
+
+// class SavingAccount : public Account {
+// private:
+//     double balance;
+
+// public:
+//     SavingAccount() { 
+//         balance = 0; 
+//     }
+
+//     void deposit(double amount) {
+//         balance += amount;
+//         cout << "Deposited: " << amount << " in Savings Account. New Balance: " << balance << endl;
+//     }
+
+//     void withdraw(double amount) {
+//         if (balance >= amount) {
+//             balance -= amount;
+//             cout << "Withdrawn: " << amount << " from Savings Account. New Balance: " << balance << endl;
+//         } else {
+//             cout << "Insufficient funds in Savings Account!\n";
+//         }
+//     }
+// };
+
+// class CurrentAccount : public Account {
+// private:
+//     double balance;
+
+// public:
+//     CurrentAccount() { 
+//         balance = 0; 
+//     }
+
+//     void deposit(double amount) {
+//         balance += amount;
+//         cout << "Deposited: " << amount << " in Current Account. New Balance: " << balance << endl;
+//     }
+
+//     void withdraw(double amount) {
+//         if (balance >= amount) {
+//             balance -= amount;
+//             cout << "Withdrawn: " << amount << " from Current Account. New Balance: " << balance << endl;
+//         } else {
+//             cout << "Insufficient funds in Current Account!\n";
+//         }
+//     }
+// };
+
+// class FixedTermAccount : public Account {
+// private:
+//     double balance;
+
+// public:
+//     FixedTermAccount() { 
+//         balance = 0;
+//     }
+
+//     void deposit(double amount) {
+//         balance += amount;
+//         cout << "Deposited: " << amount << " in Fixed Term Account. New Balance: " << balance << endl;
+//     }
+
+//     void withdraw(double amount) {
+//         throw logic_error("Withdrawal not allowed in Fixed Term Account!");
+//     }
+// };
+
+// //Client class
+// class BankClient {
+// private:
+//     vector<Account*> accounts;
+
+// public:
+//     BankClient(vector<Account*> accounts) { 
+//         this->accounts = accounts; 
+//     }
+
+//     void processTransactions() {
+//         for (Account* acc : accounts) {
+//             acc->deposit(1000);
+
+//             //Checking account type explicitly
+//             if (typeid(*acc) == typeid(FixedTermAccount)) {
+//                 cout << "Skipping withdrawal for Fixed Term Account.\n";
+//             } else {
+//                 try {
+//                     acc->withdraw(500);
+//                 } catch (const logic_error& e) {
+//                     cout << "Exception: " << e.what() << endl;
+//                 }
+//             }
+//         }
+//     }
+// };
+
+// int main() {
+//     vector<Account*> accounts;
+//     accounts.push_back(new SavingAccount());
+//     accounts.push_back(new CurrentAccount());
+//     accounts.push_back(new FixedTermAccount());
+
+//     BankClient* client = new BankClient(accounts);
+//     client->processTransactions();
+
+//     return 0;
+// }
